0f78c3ac20f232fa5d89dfd53e9e70df6d5dfcdf
rakan <Rakan.w10@outlook.com>
2023-11-27 18:25:14 +0300

    Initial Commit

---
+++ ToDoUsingSupabase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoUsingSupabase
+//
+//  Created by Rakan Alotaibi on 13/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoUsingSupabase/ToDoUsingSupabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoUsingSupabaseApp.swift
+//  ToDoUsingSupabase
+//
+//  Created by Rakan Alotaibi on 13/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ToDoUsingSupabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
